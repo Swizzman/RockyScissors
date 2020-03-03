96600d67ce74fc59fc29edcf29e14c8470a27cd4
Oliver <swizzman123@gmail.com>
2020-03-03 19:54:18 +0100

    Chat is kind off work, need REGEX

--- clientmain.cpp
+++ clientmain.cpp
@@ -1,14 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <iostream>
+#include <thread>
 /* You will to add includes here */
 
 
-// Included to get the support library
-#include <calcLib.h>
+uint16_t sockFD;
 
-int main(int argc, char *argv[]){
-  
-  /* Do magic */
-  
+void sendMsg(void)
+{
+	char msg[500];
+	while (true)
+	{
+		memset(&msg, 0, sizeof(msg));
+		uint16_t numBytes;
+		gets(msg);
+		msg[strlen(msg)] = '\n';
+		if (strlen(msg) <= 256)
+		{
+		numBytes = send(sockFD, msg, strlen(msg), 0);
+		}
+		else
+		{
+			printf("Message too long\n");
+		}
 
+	}
 }
+int main(int argc, char* argv[]) {
+
+	if (argc < 4)
+	{
+		printf("To few arguments!\n");
+		exit(0);
+	}
+	struct addrinfo hints, * serverInfo, * p;
+	uint16_t numBytes;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+	char servMsg[100];
+	uint16_t servMsg_Len = sizeof(servMsg);
+	char protocol[20] = "Chat TCP 1.0\n";
+	uint16_t returnValue;
+	if ((returnValue = getaddrinfo(argv[1], argv[2], &hints, &serverInfo)) != 0)
+	{
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(returnValue));
+		exit(0);
+	}
+	p = serverInfo;
+	for (p = serverInfo; p != NULL; p = p->ai_next) {
+		if ((sockFD = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+			perror("client: socket");
+			continue;
+		}
+		if (connect(sockFD, p->ai_addr, p->ai_addrlen) == -1)
+		{
+			printf("Error connecting: %s\n", strerror(errno));
+			close(sockFD);
+			exit(1);
+		}
+
+		break;
+	}
+	if (p == NULL) {
+		fprintf(stderr, "client: failed to create socket\n");
+		return 2;
+	}
+	freeaddrinfo(serverInfo);
+	numBytes = recv(sockFD, servMsg, servMsg_Len, 0);
+	printf("Server protocol: %s", servMsg);
+	std::thread sendingThread(sendMsg);
+	if (strcmp(servMsg, protocol) == 0)
+	{
+		printf("Protocol supported, sending nickname\n");
+		numBytes = send(sockFD, argv[3], strlen(argv[3]), 0);
+
+	}
+	//fcntl(sockFD, F_SETFL, O_NONBLOCK);
+	while (true)
+	{
+		memset(&servMsg, 0, sizeof(servMsg));
+		numBytes = recv(sockFD, servMsg, sizeof(servMsg), 0);
+
+		printf("%s", servMsg);
+
+	}
+	sendingThread.join();
+	close(sockFD);
+}
+
--- servermain.cpp
+++ servermain.cpp
@@ -1,19 +1,242 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/select.h>
 /* You will to add includes here */
 
 
-// Included to get the support library
-#include <calcLib.h>
+
 
 
 
 using namespace std;
+struct client
+{
+	char* address;
+	char nickname[100];
+	uint16_t port;
+	uint16_t socket;
+};
+int MAXCLIENTS = 10;
+int nrOfClients = 0;
+struct client** clients = new client * [MAXCLIENTS] { nullptr };
+int main(int argc, char* argv[]) {
+	if (argc < 3)
+	{
+		printf("To few arguments!");
+		exit(0);
+	}
+	/* Do more magic */
+	struct addrinfo guide, * serverInfo, * p;
+	uint16_t numBytes;
+	uint16_t sockFD;
+	uint16_t tempSFD;
+	uint8_t returnValue;
+	fd_set master;
+	fd_set read_sds;
+	int fDMax;
+	char clientMsg[350];
+	int clientMsgLen = sizeof(clientMsg);
+	char msgToSend[350];
+	struct timeval timeO;
+	timeO.tv_sec = 5;
+	timeO.tv_usec = 0;
+	memset(&guide, 0, sizeof(guide));
+	FD_ZERO(&master);
+	FD_ZERO(&read_sds);
+	guide.ai_family = AF_INET;
+	guide.ai_socktype = SOCK_STREAM;
+	guide.ai_flags = AI_PASSIVE;
+	struct sockaddr_in theirAddr;
+	char protocol[20] = "Chat TCP 1.0\n";
+
+	socklen_t theirAddr_len = sizeof(theirAddr);
+	if (argc == 3)
+	{
+
+		if ((returnValue = getaddrinfo(argv[1], argv[2], &guide, &serverInfo)) != 0)
+		{
+			fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(returnValue));
+			exit(0);
+		}
+	}
+	else if (argc == 2)
+	{
+		if ((returnValue = getaddrinfo(argv[1], argv[2], &guide, &serverInfo)) != 0)
+		{
+			fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(returnValue));
+			exit(0);
+		}
+	}
+	for (p = serverInfo; p != NULL; p = p->ai_next) {
+
+		if ((sockFD = socket(p->ai_family, p->ai_socktype,
+			p->ai_protocol)) == -1)
+		{
+			printf("listener: socket: %s\n", gai_strerror(errno));
+			continue;
+		}
+
+		if (bind(sockFD, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockFD);
+			printf("listener error: bind: %s\n", gai_strerror(errno));
+			continue;
+		}
+
+		break;
+	}
+	freeaddrinfo(serverInfo);
 
 
-int main(int argc, char *argv[]){
-  
-  /* Do more magic */
-  
+	if (listen(sockFD, 1) == -1)
+	{
+		printf("No-one listens to me:(\nError: %s", strerror(errno));
+		close(sockFD);
+		exit(0);
+	}
+	else
+	{
+		if (argc == 3)
+		{
+			printf("[x]Listening on %s:%s\n", argv[1], argv[2]);
+		}
+		else if (argc == 2)
+		{
+			printf("[x]Listening on %s:%s\n", argv[1], argv[2]);
+		}
+	}
+	FD_SET(sockFD, &master);
+	fDMax = sockFD;
+	while (true)
+	{
+		memset(&clientMsg, 0, clientMsgLen);
+		memset(&msgToSend, 0, sizeof(msgToSend));
+		read_sds = master;
+		if (select(fDMax + 1, &read_sds, NULL, NULL, &timeO) == -1)
+		{
+			printf("Select error: %s\n", strerror(errno));
+			close(sockFD);
+		}
+		for (int i = 0; i <= fDMax; i++)
+		{
+			if (FD_ISSET(i, &read_sds))
+			{
+				printf("i: %d\n", i);
+				if (i == sockFD)
+				{
+					if ((tempSFD = accept(sockFD, (struct sockaddr*) & theirAddr, &theirAddr_len)) == -1)
+					{
+						printf("Error accepting: %s\n", strerror(errno));
+					}
+					else
+					{
+						printf("Client connected from %s:%d\n", inet_ntoa(theirAddr.sin_addr), ntohs(theirAddr.sin_port));
+						clients[nrOfClients] = new client;
+						clients[nrOfClients]->address = inet_ntoa(theirAddr.sin_addr);
+						clients[nrOfClients]->port = ntohs(theirAddr.sin_port);
+						clients[nrOfClients]->socket = tempSFD;
+						numBytes = send(clients[nrOfClients]->socket, protocol, strlen(protocol), 0);
+						numBytes = recv(clients[nrOfClients]->socket, clients[nrOfClients]->nickname, sizeof(clients[nrOfClients]->nickname), 0);
 
+						printf("Client name: %s\n", clients[nrOfClients]->nickname);
+						FD_SET(clients[nrOfClients]->socket, &master);
+						if (clients[nrOfClients]->socket > fDMax)
+						{
+							fDMax = clients[nrOfClients]->socket;
+						}
+						nrOfClients++;
+					}
+				}
+				else
+				{
+					if ((numBytes = recv(i, clientMsg, clientMsgLen, 0)) <= 0)
+					{
+						if (numBytes == 0)
+						{
+							bool found = false;
+							printf("Client left\n");
+							for (int h = 0; h < nrOfClients && !found; h++)
+							{
+								if (clients[h]->socket == i)
+								{
+									delete clients[h];
+									for (int y = h; y < nrOfClients; y++)
+									{
+										if (clients[y + 1] != nullptr)
+										{
+
+											clients[y] = clients[y + 1];
+										}
+									}
+									printf("ISA EQUAL\n");
+									nrOfClients--;
+									found = true;
+								}
+							}
+
+							printf("Clients: %d\n", nrOfClients);
+
+							close(i);
+						}
+						else
+						{
+							printf("Error recieving: %s\n", strerror(errno));
+
+						}
+						close(i);
+						FD_CLR(i, &master);
+					}
+					else
+					{
+
+						struct sockaddr_in addres;
+						int sa_len;
+						sa_len = sizeof(addres);
+						getpeername(i, (struct sockaddr*) & addres, &sa_len);
+
+						char bobp[250];
+						socklen_t bl = sizeof(bobp);
+						inet_ntop(addres.sin_family, &addres.sin_addr, bobp, bl);
+						for (int i = 0; i < nrOfClients; i++)
+						{
+							if (strcmp(bobp, clients[i]->address) == 0 && ntohs(addres.sin_port) == clients[i]->port)
+							{
+								sprintf(msgToSend, "%s: %s", clients[i]->nickname, clientMsg);
+							}
+						}
+						for (int y = 0; y < nrOfClients; y++)
+						{
+
+							if (FD_ISSET(clients[y]->socket, &master))
+							{
+								if (y != sockFD && clients[y]->socket != i)
+								{
+									if ((numBytes = send(clients[y]->socket, msgToSend, strlen(msgToSend), 0)) == -1)
+									{
+										printf("Error sending\n");
+									}
+								}
+								else
+								{
+									printf("%s", msgToSend);
+
+								}
+							}
+						}
+
+					}
+				}
+			}
+		}
+	}
+	return 0;
 }
